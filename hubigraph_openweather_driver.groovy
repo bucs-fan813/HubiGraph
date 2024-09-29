1597745c0f4b9c1355ebf52f5b7b65ea05d4c1e7
bucs-fan813 <bucs-fan813@users.noreply.github.com>
2024-09-29 10:56:28 -0400

    fix WeatherTile.js uri src

--- hubigraph_openweather_driver.groovy
+++ hubigraph_openweather_driver.groovy
@@ -76,7 +76,7 @@ void pollOpenWeather() {
     
     def ParamsOWM;
     
-    state.ow_uri = 'https://api.openweathermap.org/data/3.0/onecall?lat=' + latitude + '&lon=' + longitude + '&exclude=minutely,hourly,daily,alerts&mode=json&units=imperial&appid=' + apiKey;
+    state.ow_uri = 'https://api.openweathermap.org/data/3.0/onecall?lat=' + latitude + '&lon=' + longitude + '&exclude=minutely,alerts&mode=json&units=imperial&appid=' + apiKey;
     ParamsOWM = [ uri: state.ow_uri]
     //log.debug('Poll OpenWeatherMap.org: ' + ParamsOWM)
 	asynchttpGet('openWeatherHandler', ParamsOWM)
